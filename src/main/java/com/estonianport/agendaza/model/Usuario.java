477742c06329577899005a65322a661cadc72fef
RdzSebastian <RdzSebastian@gmail.com>
2022-11-26 17:01:15 -0300

    Migracion a springboot 3.0.0 y java 17

--- src/main/java/com/estonianport/agendaza/model/Usuario.java
+++ src/main/java/com/estonianport/agendaza/model/Usuario.java
@@ -1,9 +1,5 @@
 package com.estonianport.agendaza.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,11 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import lombok.Getter;
 import lombok.Setter;
 
