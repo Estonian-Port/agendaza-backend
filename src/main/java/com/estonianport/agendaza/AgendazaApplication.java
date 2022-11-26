e3a91f208c9cbfe02c9d8215b14960ebd13673b0
RdzSebastian <RdzSebastian@gmail.com>
2022-11-26 20:46:14 -0300

    Ajustes de encriptacion

--- src/main/java/com/estonianport/agendaza/AgendazaApplication.java
+++ src/main/java/com/estonianport/agendaza/AgendazaApplication.java
@@ -3,7 +3,10 @@ package com.estonianport.agendaza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class AgendazaApplication {
 
 	public static void main(String[] args) {
