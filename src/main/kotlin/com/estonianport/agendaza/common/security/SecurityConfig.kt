0e88ea1b5e92b3f757a677b5003072b39545f13d
RdzSebastian <RdzSebastian@gmail.com>
2023-01-27 14:26:59 -0300

    Test basic auth spring security 2.7

--- src/main/kotlin/com/estonianport/agendaza/common/security/SecurityConfig.kt
+++ src/main/kotlin/com/estonianport/agendaza/common/security/SecurityConfig.kt
@@ -1,7 +1,17 @@
+package com.estonianport.agendaza.common.security
+
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -9,15 +19,45 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig {
 
     @Bean
-    @Throws(Exception::class)
-    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
-        http.csrf()
+    fun filterChain(http: HttpSecurity, authenticationManager: AuthenticationManager): SecurityFilterChain? {
+        return http
+            .csrf()
             .disable()
-            .authorizeHttpRequests{
-                    authorizeHttpRequestsConfigurer ->
-                authorizeHttpRequestsConfigurer
-                    .requestMatchers("/getUsuarioByUsernameAndContrasena").permitAll().anyRequest().authenticated()
-            }.formLogin().loginPage("/login").permitAll()
-        return http.build()
+            .authorizeHttpRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .build()
+    }
+
+    @Bean
+    fun userDetailService() : UserDetailsService {
+        val manager = InMemoryUserDetailsManager()
+        manager.createUser(
+            User.withUsername("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles()
+            .build())
+        return manager
+    }
+
+    @Bean
+    fun passwordEncoder() : PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authManager(http: HttpSecurity): AuthenticationManager? {
+        return http.getSharedObject(AuthenticationManagerBuilder::class.java)
+            .userDetailsService(userDetailService())
+            .passwordEncoder(passwordEncoder())
+            .and()
+            .build()
     }
 }
+
