0f81c15d88c98e3e3c8b40ba6dad82c94279d406
RdzSebastian <RdzSebastian@gmail.com>
2023-04-11 23:39:00 -0300

    Revierte modificaciones de entityGraph

--- src/main/kotlin/com/estonianport/agendaza/repository/EmpresaRepository.kt
+++ src/main/kotlin/com/estonianport/agendaza/repository/EmpresaRepository.kt
@@ -1,12 +1,6 @@
 package com.estonianport.agendaza.repository
 
-import com.estonianport.agendaza.model.Cargo
 import com.estonianport.agendaza.model.Empresa
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 
-interface EmpresaRepository : CrudRepository<Empresa, Long>{
-
-    @EntityGraph(attributePaths = ["listaEmpleados"])
-    override fun findAll() : List<Empresa>
-}
+interface EmpresaRepository : CrudRepository<Empresa, Long>
