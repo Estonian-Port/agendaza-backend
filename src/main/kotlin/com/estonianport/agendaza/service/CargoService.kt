d9fa6670383fb9173b0db4a9c112e6d3c6b0c1cd
RdzSebastian <RdzSebastian@gmail.com>
2023-04-11 21:29:40 -0300

    Modifica la forma de buscar cargo of usuario for agenda y cambia open in view a false, con colocando entity graph

--- src/main/kotlin/com/estonianport/agendaza/service/CargoService.kt
+++ src/main/kotlin/com/estonianport/agendaza/service/CargoService.kt
@@ -3,6 +3,7 @@ package com.estonianport.agendaza.service
 import GenericServiceImpl
 import com.estonianport.agendaza.repository.CargoRepository
 import com.estonianport.agendaza.model.Cargo
+import com.estonianport.agendaza.model.Usuario
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
@@ -15,4 +16,8 @@ class CargoService : GenericServiceImpl<Cargo, Long>() {
 
     override val dao: CrudRepository<Cargo, Long>
         get() = cargoRepository
+
+    fun findAllByUsuario(usuario : Usuario) : List<Cargo>{
+        return cargoRepository.findAllByUsuario(usuario)
+    }
 }
